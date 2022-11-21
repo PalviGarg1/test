e346dd84f8dec2fbd2140b02e908b3b4a02eb82f
Kaladhar Dwivedi <kaladhar.dwivedi@outlook.com>
2022-11-21 13:24:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import './Content/Content/login.css';
 import './App.css';
+import { Signin } from './u/Signin';
+import { Login } from './u/Login';
+import { Signup } from './u/Signup';
+import { Details } from './getstarted/Details';
+import { AccountVerifi } from './getstarted/Accountverify';
+import { CreatePassword } from './getstarted/Createpassword';
+import { CreatePasswordFromEmail } from './getstarted/Createpasswordpg';
+import { CreateProfile } from './getstarted/Createprofile';
+import { CreateProfileTchr } from './getstarted/Createprofile2';
+import { CreateProfileStu } from './getstarted/Createprofile3';
+import { Resetpsword } from './u/Resetpassword';
+import { Userprofile } from './u/Profile';
+import { Staffpage } from './u/Staff';
+import { SurveyPage } from './u/Survey';
+import { SurveyDetailsPage } from './s/SurveyDetails';
+import { SurveyDeepdivePage } from './s/SurveyDeepdive';
+import { ClassroomPage } from './u/Classroom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+
+    return (
+        <BrowserRouter>
+         
+            {/* <Headersignin /> */}
+
+            <Routes>
+                <Route path='/' element={<Login />} />
+                <Route path='/u/signin' element={<Signin />} />
+                <Route path='/u/signup' element={<Signup />} />
+                <Route path='/getstarted/details' element={<Details />} />
+                <Route path='/getstarted/accountverify' element={<AccountVerifi />} />
+                <Route path='/getstarted/createpassword' element={<CreatePassword />} />
+                <Route path='/getstarted/createpasswordpg' element={<CreatePasswordFromEmail />} />
+                <Route path='/getstarted/createprofile' element={<CreateProfile />} />
+                <Route path='/getstarted/Createprofile2' element={<CreateProfileTchr />} />
+                <Route path='/getstarted/createprofile3' element={<CreateProfileStu />} />
+                <Route path='/u/resetpassword' element={<Resetpsword />} />
+                <Route path='/u/profile' element={<Userprofile />} />
+                <Route path='/u/staff' element={<Staffpage />} />
+                <Route path='/u/survey' element={<SurveyPage />} />
+                <Route path='/s/surveydetails' element={<SurveyDetailsPage />} />
+                <Route path='/s/surveydeepdive' element={<SurveyDeepdivePage />} />
+                <Route path='/u/classroom' element={<ClassroomPage />} />
+            </Routes>
+
+                
+
+        </BrowserRouter>
+    );
 }
+
+
 
 export default App;
